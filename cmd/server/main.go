7f2c0118a40f121a8e99e7ecd87169a8a61a142d
Brady Fontenot <bfont52@gmail.com>
2021-01-08 21:52:25 -0600

    Add TLS server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,19 +2,12 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"github.com/bradyfontenot/ljw/internal/server"
 )
 
-var (
-	port = ":8080"
-)
-
 func main() {
 
-	srv := server.New(port)
-	if err := http.ListenAndServe(port, srv); err != nil {
-		log.Fatalf("Could not connect to server on port %v. \n %v", port, err)
-	}
+	srv := server.New()
+	log.Fatal(srv.ListenAndServeTLS("", ""))
 }
