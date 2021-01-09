f485e2b5afc9cce795138faec778e6bfda587e8f
Brady Fontenot <bfont52@gmail.com>
2021-01-08 21:52:50 -0600

    mTLS working - wip

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,27 +1,54 @@
 package client
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-var (
-	baseURI = "http://localhost:8080/"
+const (
+	baseURI  = "https://localhost:8080/"
+	certFile = "ssl/client.crt"
+	keyFile  = "ssl/client.key"
+	caFile   = "ssl/ca.crt"
 )
 
 // Client implements an http client
 type Client struct {
-	http.Client
-	// tr http.Transport
+	*http.Client
 }
 
 // New creates and returns a new Client
 func New() *Client {
-	client := new(Client)
 
-	return client
+	caCert, err := ioutil.ReadFile(caFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c := new(Client)
+
+	c = &Client{
+		&http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					RootCAs:      caCertPool,
+					Certificates: []tls.Certificate{cert},
+				},
+			},
+		},
+	}
+
+	return c
 }
 
 // ***TEMP****
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,16 +2,27 @@
 package server
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+const (
+	port     = ":8080"
+	certFile = "ssl/server.crt"
+	keyFile  = "ssl/server.key"
+	caFile   = "ssl/ca.crt"
+)
+
 // Server is ...
 type Server struct {
-	http.Handler
+	*http.Server
 }
 
 // Response is ...
@@ -19,26 +30,55 @@ type Response struct {
 }
 
 // New initializes a new server.
-func New(port string) *Server {
-	srv := new(Server)
+func New() *Server {
 
-	router := httprouter.New()
+	caCert, err := ioutil.ReadFile(caFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
 
-	router.GET("/", srv.Index)
-	// router.GET("/jobs")
-	// router.GET("/jobs/:id")
-	// router.POST("/jobs")
-	// router.DELETE("/jobs/:id")
-	// router.GET("/jobs/:id/log")
+	// load certificate and private key files
+	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := new(Server)
 
-	srv.Handler = router
-	return srv
+	tlsConfig := &tls.Config{
+		RootCAs:      caCertPool,
+		Certificates: []tls.Certificate{cert},
+	}
+
+	// build router and routes
+	r := httprouter.New()
+
+	r.GET("/", Index)
+	r.GET("/api/jobs", s.getJobs)
+	// r.GET("/api/jobs/:id")
+	// r.POST("/api/jobs")
+	// r.DELETE("/api/jobs/:id")
+	// r.GET("/api/jobs/:id/log")
+
+	// create https server with TLS configuration to manages certificates
+	s = &Server{
+		&http.Server{
+			Handler:   r,
+			Addr:      port,
+			TLSConfig: tlsConfig,
+		},
+	}
+	// srv.Handler = r
+	// srv.Addr = ":8080"
+	// srv.TLSConfig.Certificates = []tls.Certificate{cert}
+	return s
 }
 
 // ***TEMP***
 //
 // Index handles requests to root directory
-func (s *Server) Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Printf("Hello from root")
 	msg := map[string]string{"msg": "Hello", "name": "Brady"}
 
@@ -52,17 +92,22 @@ func (s *Server) Index(w http.ResponseWriter, r *http.Request, _ httprouter.Para
 }
 
 // get list handler /jobs
-func (s *Server) listJobs() {
+func (s *Server) getJobs(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	type response struct{}
+
+	respond()
 }
 
 // get 1 job handler /jobs/:id
-func (s *Server) jobStatus() {}
+func (s *Server) getJobStatus(w http.ResponseWriter, r *http.Request, id httprouter.Params) {}
 
 // post 1 job handler /jobs
-func (s *Server) startJob() {}
+func (s *Server) addJob(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {}
 
 // delete 1 job handler /jobs/:id
-func (s *Server) stopJob() {}
+func (s *Server) cancelJob(w http.ResponseWriter, r *http.Request, id httprouter.Params) {}
 
 // get 1 job log handler /jobs/:id/log
-func (s *Server) logJob() {}
+func (s *Server) getJobLog(w http.ResponseWriter, r *http.Request, id httprouter.Params) {}
+
+func respond() {}
