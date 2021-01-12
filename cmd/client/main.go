b6d7549415e092f06e512297dfaeec6e1759f5df
Brady Fontenot <bfont52@gmail.com>
2021-01-12 11:06:10 -0600

    Cleanup

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -8,17 +8,15 @@ import (
 
 func main() {
 
-	fmt.Print("Client Starting...\n")
-
 	c := client.New()
-
 	fmt.Print("Client started\n")
 
 	// Temporary Calls to check request/response data
+	// will be replaced with a cli
 	for i := 0; i < 5; i++ {
 		c.StartJob()
 	}
-	c.ListJobs()
+	c.ListRunningJobs()
 	c.JobStatus("3")
 	c.StopJob("3")
 	c.JobStatus("3")
