6723db9a9eea95bf43b971cb8696a88900cace3e
Brady Fontenot <bfont52@gmail.com>
2021-01-14 11:23:51 -0600

    Implement start() and stop()

--- internal/worker/job.go
+++ internal/worker/job.go
@@ -1,17 +1,17 @@
 package worker
 
 import (
+	"bytes"
 	"fmt"
-	"log"
+	"os"
 	"os/exec"
 )
 
 const (
-	running   = "RUNNING"
-	queued    = "QUEUED"
-	completed = "COMPLETED"
-	canceled  = "CANCELED"
-	failed    = "FAILED"
+	running  = "RUNNING"
+	finished = "FINISHED"
+	canceled = "CANCELED"
+	failed   = "FAILED"
 )
 
 // Job is one linux job
@@ -19,6 +19,7 @@ type Job struct {
 	cmd    string
 	status string
 	output string
+	pid    int
 }
 
 // New creates a new Job
@@ -26,7 +27,6 @@ func newJob(cmd string) *Job {
 	return &Job{
 		cmd:    cmd,
 		status: running,
-		// output: "none",
 	}
 }
 
@@ -34,27 +34,60 @@ func newJob(cmd string) *Job {
 //	* handle invalid cmd(don't kill server. just set status to Failed)
 //	* Run in go routine ya?
 
-func (j *Job) start() error {
+func (j *Job) start(id int) error {
+
 	// set job status
 	j.status = running
 
-	stdoutStderr, err := exec.Command("ls").CombinedOutput()
-	if err != nil {
-		log.Printf("%v\n", stdoutStderr)
-	}
-	fmt.Printf("%s", stdoutStderr)
+	// init new Command
+	cmd := exec.Command("sleep", "5")
 
+	// set stdout and stderr to write to same buffer
+	// mimics CombinedOutput() from os/exec library
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+	cmd.Stderr = &buf
+
+	// run command
+	if err := cmd.Start(); err != nil {
+		j.status = failed
+		j.output = err.Error()
+
+		return fmt.Errorf("error starting command: %w", err)
+	}
+	// store the Pid so stop() can be called later if needed.
+	j.pid = cmd.Process.Pid
+
+	// get stdout/stderr from buffer
+	var stdoutStderr []byte
+	buf.Write(stdoutStderr)
+
+	cmd.Wait()
+
+	// store result from process
 	j.output = string(stdoutStderr)
-	j.status = completed
+	j.status = finished
+
+	fmt.Printf("%d Finished Job # %d: %s\n", j.pid, id, stdoutStderr)
 
 	return nil
 }
 
-func (j *Job) stop() error {
+func (j *Job) stop() (bool, error) {
 
-	// kill proc
+	if j.status != running {
+		return false, nil
+	}
+
+	proc, err := os.FindProcess(j.pid)
+	if err != nil {
+		return false, fmt.Errorf("could not find process. error: %w", err)
+	}
+	if err = proc.Kill(); err != nil {
+		return false, fmt.Errorf("could not kill process. error: %w", err)
+	}
 
 	j.status = canceled
 
-	return nil
+	return true, nil
 }
