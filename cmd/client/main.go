249d28a2974f5e42792d88f234a8e584ffc27a55
Brady Fontenot <bfont52@gmail.com>
2021-01-16 12:13:55 -0600

    Add input validation and restructure

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -9,100 +10,121 @@ import (
 
 func main() {
 
-	if len(os.Args) == 1 {
-		fmt.Print("No arguments supplied. Must supply at least one argument\n")
+	if len(os.Args) <= 1 {
+		fmt.Println("\nNo arguments supplied. Must supply at least one argument\n")
+		printUsage()
 		return
 	}
 	// collect command line args
 	appCommand := os.Args[1]
 	args := os.Args[2:]
 
-	// create new client
+	// start client and attempt to connect to server
 	c, err := client.New()
 	if err != nil {
 		fmt.Printf("Problem Authenticating.\nError: %s\nShutting down...\n", err)
-		os.Exit(1)
+		return
 	}
 
 	switch appCommand {
 	case "list":
 		list(c)
 	case "status":
-		status(c, args[0])
+		status(c, args[0:])
 	case "start":
 		start(c, args[0:])
 	case "stop":
-		stop(c, args[0])
+		stop(c, args[0:])
 	case "log":
-		log(c, args[0])
+		log(c, args[0:])
+	default:
+		printUsage()
 	}
+}
+
+// printUsage prints usage instructions
+func printUsage() {
+	fmt.Println("[USAGE]")
+	fmt.Printf(" list\n start \t<linux cmd>\n status\t<job id>\n stop \t<job id>\n log \t<job id>\n\n")
 }
 
 func list(c *client.Client) {
 	err := c.ListRunningJobs()
 	if err != nil {
 		fmt.Printf("Error: %v \n", err)
-		os.Exit(1)
+		return
 	}
-
 }
 
-func status(c *client.Client, id string) {
-	err := c.JobStatus(id)
+func status(c *client.Client, args []string) {
+	// validate only one arg supplied for id.
+	id, err := processID(args)
+	if err != nil {
+		return
+	}
+
+	err = c.JobStatus(id)
 	if err != nil {
 		fmt.Printf("Error: %v \n", err)
-		os.Exit(1)
+		return
 	}
 }
 
 func start(c *client.Client, args []string) {
+	// validate args exist
+	if len(args) < 1 {
+		fmt.Println("\nNo linux command supplied. Must supply a command\n")
+		printUsage()
+		return
+	}
+
+	//
 	err := c.StartJob(args)
 	if err != nil {
 		fmt.Printf("Error: %v \n", err)
-		os.Exit(1)
+		return
 	}
 }
 
-func stop(c *client.Client, id string) {
-	err := c.StopJob(id)
+func stop(c *client.Client, args []string) {
+	// validate only one arg supplied for id
+	id, err := processID(args)
+	if err != nil {
+		return
+	}
+
+	err = c.StopJob(id)
 	if err != nil {
 		fmt.Printf("Error: %v \n", err)
-		os.Exit(1)
+		return
 	}
 }
 
-func log(c *client.Client, id string) {
-	err := c.GetJobLog(id)
+func log(c *client.Client, args []string) {
+	// validate only one arg supplied for id.
+	id, err := processID(args)
+	if err != nil {
+		return
+	}
+
+	err = c.GetJobLog(id)
 	if err != nil {
 		fmt.Printf("Error: %v \n", err)
-		os.Exit(1)
+		return
 	}
 }
 
-//########################## RAW FUNCTIONS #########################################
+// processID ensures only one arg was supplied for id
+func processID(args []string) (string, error) {
+	if len(args) < 1 {
+		fmt.Println("\nNo id supplied.\n")
+		printUsage()
+		return "", errors.New("no argument supplied")
+	} else if len(args) > 1 {
+		fmt.Println("\nToo many args or id has spaces. Please Supply only one id at a time.\n")
+		printUsage()
+		return "", errors.New("too many arguments supplied. only needs 1")
+	}
 
-// func main() {
-
-// 	c, err := client.New()
-// 	if err != nil {
-// 		fmt.Printf("Problem Authenticating.\nError: %s\nShutting down...\n", err)
-// 		os.Exit(1)
-// 	}
-
-// 	fmt.Print("Client started\n")
-// 	cmd := make([]string, 0)
-// 	cmd = append(cmd, "sleep")
-// 	cmd = append(cmd, "3")
-
-// 	// Temporary Calls to check request/response data
-// 	// will be replaced with a cli
-// 	for i := 0; i < 5; i++ {
-// 		c.StartJob(cmd)
-// 		time.Sleep(time.Duration(1) * time.Second)
-// 	}
-// c.ListRunningJobs()
-// 	c.JobStatus("3")
-// 	c.StopJob("3")
-// 	c.JobStatus("3")
-// 	c.GetJobLog("3")
-// }
+	return args[0], nil
+}
