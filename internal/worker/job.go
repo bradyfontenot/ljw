cbe2d1c5d529ba3a68738820be31664962b248cf
Brady Fontenot <bfont52@gmail.com>
2021-01-17 17:31:37 -0600

    Refactor start()

--- internal/worker/job.go
+++ internal/worker/job.go
@@ -2,10 +2,11 @@ package worker
 
 import (
 	"bytes"
+	"context"
 	"fmt"
-	"os"
 	"os/exec"
 	"sync"
+	"syscall"
 	"time"
 )
 
@@ -16,6 +17,7 @@ const (
 	finished = "FINISHED"
 	canceled = "CANCELED"
 	failed   = "FAILED"
+	timeout  = "TIMEOUT"
 )
 
 // job is one linux job
@@ -37,27 +39,27 @@ func newJob(cmd []string) *job {
 
 // start handles running of linux command processes in a go routine
 func (j *job) start(id string) error {
-	// TODO: set timeout in case process hangs
-	// exec.CommandContext() seems to be the suggested way to handle this?
 
-	// channel to catch error inside go routine
-	errCh := make(chan error)
-
-	// run process
 	go func() {
-		// create new Command
-		cmd := exec.Command(j.cmd[0], j.cmd[1:]...)
 
-		// set stdout and stderr to write to same buffer
-		// mimics (*cmd) CombinedOutput() from os/exec library
+		// timeout
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		// init command
+		cmd := exec.CommandContext(ctx, j.cmd[0], j.cmd[1:]...)
+		// Create a Process Group ID so call to kill also kills child process
+		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+		// set stdout and stderr to write to single buffer
 		var buf bytes.Buffer
 		cmd.Stdout = &buf
 		cmd.Stderr = &buf
-
 		// run command
 		err := cmd.Start()
-		errCh <- err
 		if err != nil {
+			cancel()
+			j.Lock()
+			j.output = err.Error()
+			j.status = failed
+			j.Unlock()
 			return
 		}
 
@@ -67,32 +69,36 @@ func (j *job) start(id string) error {
 		j.pid = cmd.Process.Pid
 		j.Unlock()
 
-		cmd.Wait()
-
+		// Blocks until process finishes or is killed
+		err = cmd.Wait()
+		cancel()
 		j.Lock()
 		defer j.Unlock()
-		// store result from process
+
+		// record stdout/stderr
 		j.output = string(buf.Bytes())
+		fmt.Println(err) // debug temp
+
+		// handle premature exit.
+		// tbh not sure most reliable way to check exit was due to kill signal.
+		// technically -1 can mean terminated by signal or that process hasn't exited
+		// but if Wait() is no longer blocking then should be finished or killed.
+		// also consider `err.Error() == "signal: killed"`` instead of exit code:
+		// not sure if there's a way to compare based on Signal type?
+		if cmd.ProcessState.ExitCode() == -1 && ctx.Err() != context.DeadlineExceeded {
+			j.status = canceled
+			return
+		} else if ctx.Err() == context.DeadlineExceeded {
+			j.status = timeout
+			return
+		}
 
 		if cmd.ProcessState.Success() {
 			j.status = finished
+		} else {
+			j.status = failed
 		}
-		// BUG: prevents stop() from setting status to canceled when called.
-		//  }else {
-		// 	j.status = failed
-		// }
 	}()
-
-	j.Lock()
-
-	// handle error from cmd.Start() in Go routine
-	err := <-errCh
-	if err != nil {
-		j.output = fmt.Errorf("error starting command: %v", err).Error()
-		j.status = failed
-		return err
-	}
-	j.Unlock()
 
 	// small delay to let go routine finish writing output for fast executing commands.
 	// allows output to be returned on initial response to starting a job
@@ -103,24 +109,17 @@ func (j *job) start(id string) error {
 
 // stop kills process if it is running when called.
 func (j *job) stop() (bool, error) {
-	j.Lock()
-	defer j.Unlock()
+	j.RLock()
+	defer j.RUnlock()
 
 	if j.status != running {
 		return false, nil
 	}
 
-	// lookup by pid and kill when found
-	proc, err := os.FindProcess(j.pid)
-	if err != nil {
-		return false, fmt.Errorf("could not find process. error: %v", err)
-	}
-
-	if err = proc.Kill(); err != nil {
+	// kill process group
+	if err := syscall.Kill(-j.pid, syscall.SIGKILL); err != nil {
 		return false, fmt.Errorf("could not kill process. error: %v", err)
 	}
-
-	j.status = canceled
 
 	return true, nil
 }
