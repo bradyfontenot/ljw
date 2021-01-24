668e58c1e491bf8d144f630adda26b81499a316d
Brady Fontenot <bfont52@gmail.com>
2021-01-23 22:41:50 -0600

    Write output to log realtime

--- internal/worker/job.go
+++ internal/worker/job.go
@@ -1,10 +1,10 @@
 package worker
 
 import (
-	"bytes"
-	"context"
+	"bufio"
 	"fmt"
 	"os/exec"
+	"strings"
 	"sync"
 	"syscall"
 	"time"
@@ -17,13 +17,12 @@ const (
 	finished = "FINISHED"
 	canceled = "CANCELED"
 	failed   = "FAILED"
-	timeout  = "TIMEOUT"
 )
 
 type job struct {
 	cmd    []string
 	status string
-	output string
+	output []string
 	pid    int
 	sync.RWMutex
 }
@@ -37,41 +36,47 @@ func newJob(cmd []string) *job {
 
 // start handles running of linux command processes
 func (j *job) start() {
+	cmdString := strings.Join(j.cmd[:], " ")
+	cmd := exec.Command("sh", "-c", cmdString)
+	// Create a Process Group ID so call to terminate also kills child process
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	cmdReader, err := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
 
+	done := make(chan int)
+	scanner := bufio.NewScanner(cmdReader)
 	go func() {
-
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		cmd := exec.CommandContext(ctx, j.cmd[0], j.cmd[1:]...)
-		// Create a Process Group ID so call to kill also kills child process
-		cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-
-		var buf bytes.Buffer
-		cmd.Stdout = &buf
-		cmd.Stderr = &buf
-
-		err := cmd.Start()
-		if err != nil {
-			cancel()
+		for scanner.Scan() {
+			line := scanner.Text()
 			j.Lock()
-			j.output = err.Error()
-			j.status = failed
+			j.output = append(j.output, line+"\n")
 			j.Unlock()
-			return
 		}
+		done <- 1
+	}()
 
+	err = cmd.Start()
+	if err != nil {
 		j.Lock()
-		j.status = running
-		// store the Pid so stop() can be called later if needed.
-		j.pid = cmd.Process.Pid
+		j.output = append(j.output, err.Error())
+		j.status = failed
 		j.Unlock()
+		return
+	}
 
+	j.Lock()
+	j.status = running
+	// store the Pid so stop() can be called later if needed.
+	j.pid = cmd.Process.Pid
+	j.Unlock()
+	go func() {
+		<-done
 		err = cmd.Wait()
-		cancel()
 		j.Lock()
 		defer j.Unlock()
-
-		j.output = string(buf.Bytes())
-		fmt.Println(err) // debug temp
+		if err != nil {
+			j.output = append(j.output, "Error: "+err.Error())
+		}
 
 		// handle premature exit.
 		// tbh not sure most reliable way to check exit was due to kill signal.
@@ -79,25 +84,17 @@ func (j *job) start() {
 		// but if Wait() is no longer blocking then proc should be finished or killed.
 		// Also considered using `err.Error() == "signal: killed"`` instead of exit code:
 		// not sure if there's a way to grab the signal sent and compare based on Signal type?
-		if cmd.ProcessState.ExitCode() == -1 && ctx.Err() != context.DeadlineExceeded {
+		if cmd.ProcessState.ExitCode() == -1 {
 			j.status = canceled
 			return
-		} else if ctx.Err() == context.DeadlineExceeded {
-			j.status = timeout
-			return
 		}
-
 		if cmd.ProcessState.Success() {
 			j.status = finished
 		} else {
 			j.status = failed
 		}
 	}()
-
-	// small delay to let go routine finish writing output for fast executing commands.
-	// allows output to be returned on initial response to starting a job
-	time.Sleep(50 * time.Millisecond)
-
+	time.Sleep(15 * time.Millisecond)
 }
 
 // stop kills process if it is running when called.
@@ -109,8 +106,8 @@ func (j *job) stop() (bool, error) {
 		return false, nil
 	}
 
-	if err := syscall.Kill(-j.pid, syscall.SIGKILL); err != nil {
-		return false, fmt.Errorf("could not kill process. error: %v", err)
+	if err := syscall.Kill(-j.pid, syscall.SIGTERM); err != nil {
+		return false, fmt.Errorf("could not terminate process. error: %v", err)
 	}
 
 	return true, nil
@@ -128,7 +125,7 @@ func (j *job) Status() string {
 	return j.status
 }
 
-func (j *job) Output() string {
+func (j *job) Output() []string {
 	j.RLock()
 	defer j.RUnlock()
 	return j.output
