8c8f17c29459518cb6317af4cda6e5ebc655d070
Brady Fontenot <bfont52@gmail.com>
2021-01-24 22:17:31 -0600

    cleanup

--- internal/server/router.go
+++ internal/server/router.go
@@ -23,12 +23,12 @@ import (
 // I could pass a single type into the sendResp() helper that marshals the json and writes the request.
 // I wanted to avoid some repetitive boilerplate in the handlers.
 type Response struct {
-	Success bool     `json:"success,omitempty"` // successful operation
-	ID      string   `json:"id,omitempty"`      // job ID
-	Status  string   `json:"status,omitempty"`  // job status
-	Cmd     string   `json:"cmd,omitempty"`     // job command
-	Output  string   `json:"output,omitempty"`  // job output
-	IDList  []string `json:"idList,omitempty"`  // list of job ID's
+	Success bool     `json:"success,omitempty"`
+	ID      string   `json:"id,omitempty"`
+	Status  string   `json:"status,omitempty"`
+	Cmd     string   `json:"cmd,omitempty"`
+	Output  string   `json:"output,omitempty"`
+	IDList  []string `json:"idList,omitempty"`
 }
 
 // router creates handler and defines the routes.
@@ -105,9 +105,6 @@ func (s *Server) stopJob(w http.ResponseWriter, r *http.Request, p httprouter.Pa
 	w.WriteHeader(http.StatusOK)
 
 	// build response msg & send
-	//
-	// probably should include reason if job isn't cancelled b/c
-	// it's already complete so client knows why job wasn't cancelled.
 	resp := Response{
 		Success: result,
 	}
